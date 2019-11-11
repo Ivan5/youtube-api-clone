c7630b3887d84e7473c1539252c663572b2a5370
Ivan5 <converse_ivan@hotmail.com>
2019-11-11 16:37:47 -0600

    Make a components to use in the project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from "react";
+import { Grid } from "@material-ui/core";
+import { SearchBar, VideoList, VideoDetail } from "./components";
 
-/*class App extends React.Component {
+import youtube from "./api/youtube";
+
+class App extends React.Component {
   render() {
-    return <h1>Youtube Clone App</h1>;
+    return (
+      <Grid justify="center" container spacing={16}>
+        <Grid item xs={12}>
+          <Grid container spacing={16}>
+            <Grid item xs={12}>
+              <SearchBar />
+            </Grid>
+            <Grid item xs={8}>
+              <VideoDetail />
+            </Grid>
+            <Grid item xs={4}>
+              {/*Video List */}
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    );
   }
-}*/
-
-const App = () => {
-  return <h1>Youtube Clone App</h1>;
-};
+}
 
 export default App;
